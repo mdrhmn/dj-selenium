974f98c210d3c2d8311e3a59f3e16c66968953d5
Muhd Rahiman <50654608+mdrhmn@users.noreply.github.com>
2021-02-25 05:18:49 +0800

    Code housekeeping and added dummy .settings.env file

--- main/views.py
+++ main/views.py
@@ -29,36 +29,21 @@ def autologin(driver, url, username, password):
 
 
 def scrap(request):
-    # browser = webdriver.Chrome()
-    # browser.get('http://seleniumhq.org/')
-
+    
     options = Options()
     options.headless = True
     options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(options=options)
-    # driver.get("https://maya.um.edu.my/sitsvision/wrd/siw_lgn")
 
     USERNAME = os.getenv("USERNAME")
     PASSWORD = os.getenv("PASSWORD")
 
-    # login = driver.find_element_by_id('MUA_CODE.DUMMY.MENSYS').send_keys(USERNAME)
-    # password = driver.find_element_by_id('PASSWORD.DUMMY.MENSYS').send_keys(PASSWORD)
-    # submit = driver.find_element_by_xpath("//input[@value='Log in']").click()
-
     autologin(driver, 'https://maya.um.edu.my/sitsvision/wrd/siw_lgn',
               USERNAME, PASSWORD)
 
-    # try:
-    #     logout_button = driver.find_element_by_xpath("//a[@target='_self']")
-    #     print('Successfully logged in')
-    # except NoSuchElementException:
-    #     print('Incorrect login/password')
-
     timetable_popup = driver.find_element_by_xpath(
         "//a[@href='javascript:timetable_popup();']").click()
-    # search_timetable = driver.find_element_by_xpath(
-    #     "//a[@role='button' and .//span[@class='glyphicon glyphicon-search']]").click()
 
     search_timetable = driver.find_element_by_xpath(
         "//*[@id='sits_dialog']/center/div/div/div[3]/a").click()
@@ -76,38 +61,11 @@ def scrap(request):
     select_faculty_dropdown = driver.find_element_by_xpath("//*[@id='" + login_id_modified + "_3_1_chosen']/a").click()
     select_faculty = driver.find_element_by_xpath("//*[@id='" + login_id_truncated + ".3-121']").click()
 
-    # select_year = driver.find_element_by_id(login_id_modified + "_1_1_chosenspan")
-    # driver.execute_script("arguments[0].innerText = '2020/2021'", select_year)
-    # select_year = driver.find_element_by_id(login_id_truncated + ".1-111")
-    # driver.execute_script("arguments[0].setAttribute('class', 'active-result result-selected')", select_year)
-    # select_semester = driver.find_element_by_id(login_id_modified + "_2_1_chosenspan")
-    # driver.execute_script("arguments[0].innerText = 'SEMESTER 2'", select_semester)
-    # select_faculty = driver.find_element_by_id(login_id_modified + "_3_1_chosenspan")
-    # driver.execute_script("arguments[0].innerText = 'FACULTY OF COMPUTER SCIENCE AND INFORMATION TECHNOLOGY'", select_faculty)
-
     select_campus = Select(driver.find_element_by_id(login_id.removesuffix('.1-1') + '.5-1'))
     select_campus.select_by_visible_text('UNIVERSITI MALAYA KUALA LUMPUR')
 
     submit_timetable = driver.find_element_by_xpath("//*[@id='poddatasection']/div[2]/div[3]/div/input[3]").click()
 
-    # table_element = driver.find_element_by_xpath("//*[@id='DataTables_Table_0_wrapper']")
-    # for tr in table_element.find_elements_by_tag_name('tr'):
-    #     print(tr.text)
-
-    # while True:
-    #     try:
-    #         # element = driver.find_element_by_css_selector('a[data-dt-idx="7"]')
-    #         # driver.execute_script("arguments[0].click();", element)
-    #         # next_page = driver.find_element_by_xpath("//*[@id='DataTables_Table_0_next']/a").click()
-    #         # WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='DataTables_Table_0_next']/a"))).click()
-    #         # WebDriverWait(driver, 10).until(EC.element_to_be_clickable(driver.execute_script("arguments[0].click();", element))).click()
-    #         driver.execute_script("arguments[0].click();", WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='DataTables_Table_0_next']"))))
-    #         print("Clicked on Next Page »")
-    #     except TimeoutException:
-    #         print("No more Next Page »")
-    #         break
-
-    # element = driver.find_element_by_xpath("//*[@id='DataTables_Table_0_next']/a")
     last_page = driver.find_element_by_xpath("//*[@id='DataTables_Table_0_last']/a")
     driver.execute_script("arguments[0].click();", last_page)
     last_page_num = driver.find_element_by_xpath("//*[@id='DataTables_Table_0_paginate']/ul/li[7]/a").get_attribute('text')
@@ -134,9 +92,6 @@ def scrap(request):
         driver.execute_script("arguments[0].click();", next_page)
 
     f.close()
-
-    # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    # driver.save_screenshot('screenshot.png')
 
     driver.quit()
     messages.success(request, "Timetable extraction successful.")
